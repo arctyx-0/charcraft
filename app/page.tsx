29c470314e4f33f8aff9ce11cf97f4bfa18c5ad0
Jett Gentes <146028226+arctyx-0@users.noreply.github.com>
2026-04-22 10:18:31 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='bg-[url(photos/char-1.jpg)] size-dvh'>
+    <div className='bg-[url(photos/char-1.jpg)] bg-contain'>
       
     </div>
   );
