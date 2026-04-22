bcf3d916aea01c31c292eaa4178fd54ad0a12195
Jett Gentes <146028226+arctyx-0@users.noreply.github.com>
2026-04-22 10:25:37 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='bg-[url(photos/char-1.jpg)] bg-contain bg-repeat-round'>
       <div className='h-dvh w-dvh'>
-        <h1>Charcraft</h1>
+        <p className='text-2xl'>Charcraft</p>
       </div>
     </div>
   );
