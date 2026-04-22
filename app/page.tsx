d4e1eee9fc4bc3faf2f6652ed81139746b1197ea
Jett Gentes <146028226+arctyx-0@users.noreply.github.com>
2026-04-22 10:36:18 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='bg-slate-800'>
       <div className='h-dvh w-dvh relative'>
-        <p className='text-2xl top-0 inset-x-0 absolute'>Charcraft</p>
+        <p className='text-2xl absolute top-0'>Charcraft</p>
       </div>
     </div>
   );
