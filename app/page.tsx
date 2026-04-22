e093827a50aecae043e98bf651f3e3dadca2a3a3
Jett Gentes <146028226+arctyx-0@users.noreply.github.com>
2026-04-22 10:20:53 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className='bg-[url(photos/char-1.jpg)] bg-contain'>
-      
+      <div className='h-full w-full'>
+        <h1>Charcraft</h1>
+      </div>
     </div>
   );
 }
