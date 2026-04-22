b9f0f2e7e7d8973511633464f867995fb1ed8d57
Jett Gentes <146028226+arctyx-0@users.noreply.github.com>
2026-04-22 10:22:33 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className='bg-[url(photos/char-1.jpg)] bg-contain'>
-      <div className='h-full w-full'>
+      <div className='h-dvh w-dvh'>
         <h1>Charcraft</h1>
       </div>
     </div>
