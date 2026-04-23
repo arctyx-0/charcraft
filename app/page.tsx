836583e5c3b909138bd51b69de020c11a9322e53
Jett Gentes <146028226+arctyx-0@users.noreply.github.com>
2026-04-23 10:07:09 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='bg-[url(photos/char-1.jpg)] bg-size-[auto-50px] bg-repeat bg-repeat-round'>
+    <div className='bg-[url(photos/char-1.jpg)] bg-size-[10px] bg-repeat bg-repeat-round'>
       <div className='h-dvh w-dvh'>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
       </div>
